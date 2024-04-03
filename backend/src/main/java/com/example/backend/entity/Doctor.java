33248d479dda90c4db3ad890fb1c2984c9b4d535
TejaJanakiRam <nagateja.janakiram@iiitb.ac.in>
2024-04-03 17:59:27 +0530

    Added doctor entity in backend

--- backend/src/main/java/com/example/backend/entity/Doctor.java
+++ backend/src/main/java/com/example/backend/entity/Doctor.java
@@ -1,5 +1,29 @@
 package com.example.backend.entity;
 
-public class Doctor {
-    
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Doctor")
+@Getter
+@Setter
+public class Doctor extends User{
+    // private Long PatientId;
+    @OneToOne
+   @JoinColumn(name="Org_ID",referencedColumnName = "id")
+   private Organization orgid;
+
+   @OneToOne
+   @JoinColumn(name="Spec_ID",referencedColumnName = "id")
+   private Speciality specid;
+   
+    private Date dob;
+    private USER_GENDER gender = USER_GENDER.OTHER;
+    private String address;
 }
