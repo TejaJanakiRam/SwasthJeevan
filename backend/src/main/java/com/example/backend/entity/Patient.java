c74fbd1957908e61c5ce1f98a47d4f439f9a1074
shag1802 <agrawalshaurya2002@gmail.com>
2024-04-03 14:52:33 +0530

    Backend_schema_second

---
+++ backend/src/main/java/com/example/backend/entity/Patient.java
@@ -0,0 +1,14 @@
+package com.example.backend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Patient")
+@Getter
+@Setter
+public class Patient extends User{
+    
+}
--- backend/src/main/java/com/example/backend/entity/User.java
+++ backend/src/main/java/com/example/backend/entity/User.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -15,7 +16,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Inheritance(strategy=InheritanceType.TABLE_CLASS)
+@Inheritance(strategy = InheritanceType.JOINED)
 @Table(name = "user")
 @Getter
 @Setter
