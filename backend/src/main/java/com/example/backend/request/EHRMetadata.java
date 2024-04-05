178c97bed470726707600ae1889ce1fc134bda6d
aniruddhac13 <aniruddha.chaudhari@iiitb.org>
2024-04-05 23:20:33 +0530

    Added EHRMetadata as a request structure

---
+++ backend/src/main/java/com/example/backend/request/EHRMetadata.java
@@ -0,0 +1,20 @@
+package com.example.backend.request;
+
+import java.util.Date;
+
+import com.example.backend.entity.EHR_TYPE;
+import com.example.backend.types.DIAGNOSIS_TYPE;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class EHRMetadata {
+    private Long id = null;
+    private Long userId;
+    private EHR_TYPE type;
+    private DIAGNOSIS_TYPE diagnosisType;
+    private Date issueDate;
+    private Date endDate;
+}
