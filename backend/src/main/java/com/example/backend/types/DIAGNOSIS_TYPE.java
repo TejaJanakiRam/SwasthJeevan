231e35a4bc77c8a5af23c9e40ce44c48c3949268
aniruddhac13 <aniruddha.chaudhari@iiitb.org>
2024-04-05 23:17:25 +0530

    Added diagnosis as a type

---
+++ backend/src/main/java/com/example/backend/types/DIAGNOSIS_TYPE.java
@@ -0,0 +1,11 @@
+package com.example.backend.types;
+
+public enum DIAGNOSIS_TYPE {
+    VIRAL_INFECTION,
+    LOW_SUGAR,
+    HIGH_SUGAR,
+    LOW_BP,
+    HIGH_BP,
+    HYPO_THYROIDISM,
+    HYPER_THYROIDISM
+}
